5c3a49cafecd1e95bd1f24bc5cdc785b88533c43
Kareem Morsy <kreem.morsy@hotmail.com>
2021-02-10 23:32:40 +0200

    Used port from config

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const config = require('config');
+const bodyParser = require('body-parser');
 const cn = require("./app/controllers/controllers.js");
 const db = require('./app/data_control/db.js').Database;
 
-let database = db.getInstance(); 
+let database = db.getInstance();
 const app = express();
 
 // parse requests of content-type: application/json
@@ -14,17 +15,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./app/routes/routes.js")(app);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000.");
-  });
-  
+app.listen(config.get('app.port'), () => {
+  console.log(`Server is running on port ${config.get('app.port')}.`);
+});
+
 setInterval(() => {
-  time=Math.round(new Date().getTime()/1000);
-  for(let i=0;i<database.buses.lingth;i++){
-    if(database.buses[i].time==null){
+  time = Math.round(new Date().getTime() / 1000);
+  for (let i = 0; i < database.buses.lingth; i++) {
+    if (database.buses[i].time == null) {
     }
-    else if (database.buses[i].time<time-5){
+    else if (database.buses[i].time < time - 5) {
       //send an alert.
     }
   }
-},6000);
+}, 6000);
