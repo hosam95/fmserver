3f3d8733d8c70b98a555dc7946987dc008afae4c
hosam95 <hosamsaeid95@gmail.com>
2022-09-25 19:02:17 +0200

    DB core code

---
+++ app/data_control/CRUD.js
@@ -0,0 +1,69 @@
+module.exports.CRUD= class CRUD{
+    collections=["cars","c_users","c_drivers","rides","places"]
+    async create(collection,val){
+        if(!this.#check_collection(collection)){
+            return false
+        }
+        const db=await MongoClient.connect(dbUri)
+        var dbo = db.db(dbName);
+        
+        await dbo.collection(collection).insertOne(val, function(err, res) {
+        if (err) throw err;
+        });
+
+        db.close();
+        return true;
+    }
+
+    async read(collection,query,page=1,limit=50){
+        if(!this.#check_collection(collection)){
+            return false
+        }
+        const db=await MongoClient.connect(dbUri)
+        var dbo = db.db(dbName);
+        
+        let vals=await dbo.collection(collection).find(query)
+        .skip( page > 0 ? ( ( page - 1 ) * limit ) : 0 )
+        .limit( limit ).toArray()
+
+        db.close();
+        return vals;
+    }
+
+    async update(collection,query,val){
+        if(!this.#check_collection(collection)){
+            return false
+        }
+        const db=await MongoClient.connect(dbUri)
+        var dbo = db.db(dbName);
+
+        dbo.collection(collection).updateOne(query, {...val,id:query.id}, function (err, res) {
+            if (err) throw err;
+      
+            db.close();
+            return true
+        });
+    }
+
+    async delete(collection,query){
+        if(!this.#check_collection(collection)){
+            return false
+        }
+        const db=await MongoClient.connect(dbUri)
+        var dbo = db.db(dbName);
+
+        dbo.collection(collection).deleteOne(query, function (err, res) {
+            if (err) throw err;
+    
+            db.close();
+            return true
+        });
+    }
+
+    #check_collection(collection){
+        if(collection in this.collections){
+            return true
+        }
+        return false;
+    }
+}
