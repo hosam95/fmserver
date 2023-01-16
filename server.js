b69351cbcb17816460a392336770976c43a304b6
hosam95 <hosamsaeid95@gmail.com>
2023-01-16 20:33:53 +0200

    re megrate ro https

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const https=require('https');
+const fs=require('fs');
 const config = require('config');
 var cors = require('cors')
 const bodyParser = require('body-parser');
@@ -30,11 +32,21 @@ app.use(cors(corsOptions))
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./app/routes/routes.js")(app);
 
-app.listen(config.get('app.port'), () => {
-  console.log(`Server is running on port ${config.get('app.port')}.`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(config.get('app.port'), ()=>{
+    console.log(`Server is running on port ${config.get('app.port')}.`)
+  }
+);
+
+require("./app/routes/routes.js")(app);
 
 setInterval(() => {
   time = Math.round(new Date().getTime() / 1000);
